b671ada6606ddb9a8fb6da60c7589a4943f8a785
Steven Campbell <user@example.co>
2017-01-21 15:06:09 -0500

    add lift code

--- src/com/subsystem/Lift.java
+++ src/com/subsystem/Lift.java
@@ -1,10 +1,25 @@
 package com.subsystem;
 
 import edu.wpi.first.wpilibj.Joystick;
+import com.ctre.CANTalon;
+import edu.wpi.first.wpilibj.AnalogInput;
 
 public class Lift extends Manipulator {
 	
+	CANTalon lift;
+	AnalogInput pot;
+	
 	public Lift(Joystick ps3, Joystick stick2) {
 		super(ps3, stick2);
+		lift = new CANTalon(5);
+		pot = new AnalogInput(0);
+	}
+	
+	public double getPot() {
+		return pot.getVoltage();
+	}
+	
+	public void lift() {
+		lift.set(stick2.getRawAxis(1));
 	}
 }
