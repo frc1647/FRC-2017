3e4d83e35ff85ee882ee706ee78a5542a6885141
Steven Campbell <user@example.co>
2017-02-13 18:40:35 -0500

    add messenger for light sensor

--- src/com/subsystem/Messenger.java
+++ src/com/subsystem/Messenger.java
@@ -2,15 +2,22 @@ package com.subsystem;
 
 import edu.wpi.first.wpilibj.smartdashboard.*;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.networktables.*;
 
 public class Messenger {
 	boolean gear;
 	boolean openGearDoor;
 	boolean pushGearDoor;
 	double liftPot;
+	NetworkTable vision;
+	boolean switch1;
+	boolean switch2;
+	boolean switch3;
+	double colorSensor;
 	
 	public Messenger() {
 		DriverStation.getInstance();
+		vision = NetworkTable.getTable("vision");
 	}
 	
 	public void putData() {
@@ -19,15 +26,39 @@ public class Messenger {
 			SmartDashboard.putBoolean("Gear Door Open/Close", openGearDoor);
 			SmartDashboard.putString("Push Gear Door", pushGearDoor ? "retracted" : "extended");
 			SmartDashboard.putNumber("Lift Pot", liftPot);
+			SmartDashboard.putNumber("Color Sensor", colorSensor);
 		} catch (Exception e) {
 			System.out.println("Error with SmartDashboard");
 		}
 	}
 	
-	public void setData(boolean gear, boolean openGearDoor, boolean pushGearDoor, double liftPot) {
+	public void setData(boolean gear, boolean openGearDoor, boolean pushGearDoor, double liftPot, double colorSensor) {
 		this.gear = gear;
 		this.openGearDoor = openGearDoor;
 		this.pushGearDoor = pushGearDoor;
 		this.liftPot = liftPot;
+		this.colorSensor = colorSensor;
+	}
+	
+	public void setData(boolean switch1, boolean switch2, boolean switch3){
+		this.switch1 = switch1;
+		this.switch2 = switch2;
+		this.switch3 = switch3;
+	}
+	
+	public void putAutoData(){
+		try {
+			SmartDashboard.putBoolean("switch 1", switch1);
+			SmartDashboard.putBoolean("switch 2", switch2);
+			SmartDashboard.putBoolean("switch 3", switch3);
+		} catch (Exception e) {
+			System.out.println("Error with SmartDashboard");
+		}
+	}
+	
+	public NetworkTable getVisionData() {
+		// TODO: format data in a more useable way
+		
+		return vision;
 	}
 }
