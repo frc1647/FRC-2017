a6057094fd9f57fa3d20d5a64bcb183117f02d96
Steven Campbell <user@example.co>
2017-02-13 18:40:56 -0500

    add light sensor code

--- src/com/subsystem/Lift.java
+++ src/com/subsystem/Lift.java
@@ -9,18 +9,29 @@ public class Lift extends Manipulator {
 	// TODO: auto operation
 	CANTalon lift;
 	AnalogInput pot;
+	AnalogInput colorSensor;
 	
 	public Lift(Joystick ps3, Joystick stick2) {
 		super(ps3, stick2);
 		lift = new CANTalon(5);
 		pot = new AnalogInput(0);
+		colorSensor = new AnalogInput(3);
 	}
 	
 	public double getPot() {
 		return pot.getVoltage();
 	}
 	
+	public double getColorSensor() {
+		return colorSensor.getVoltage();
+	}
+	
+	public double getEncoderPosition() {
+		return lift.getEncPosition(); 
+	}
+	
 	public void lift() {
 		lift.set(stick2.getRawAxis(1));
+		System.out.println("Lift Enc x: " + getEncoderPosition());
 	}
 }
