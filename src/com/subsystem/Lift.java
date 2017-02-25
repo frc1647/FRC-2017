fb3a199d21a603cee678a86bb6b798e8f1912b85
Steven Campbell <user@example.co>
2017-02-24 22:18:45 -0500

    add code reflecting physical robot attributes

--- src/com/subsystem/Lift.java
+++ src/com/subsystem/Lift.java
@@ -3,27 +3,30 @@ package com.subsystem;
 import edu.wpi.first.wpilibj.Joystick;
 import com.ctre.CANTalon;
 import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 public class Lift extends Manipulator {
-	// TODO: ask about sensor/limit switch for top
+
 	// TODO: auto operation
 	CANTalon lift;
 	AnalogInput pot;
-	AnalogInput colorSensor;
+	AnalogInput lightSensor;
+	DigitalInput limitSwitch1;
+	DigitalInput limitSwitch2;
 	
 	public Lift(Joystick ps3, Joystick stick2) {
 		super(ps3, stick2);
 		lift = new CANTalon(5);
 		pot = new AnalogInput(0);
-		colorSensor = new AnalogInput(3);
+		lightSensor = new AnalogInput(3);
 	}
 	
 	public double getPot() {
 		return pot.getVoltage();
 	}
 	
-	public double getColorSensor() {
-		return colorSensor.getVoltage();
+	public double getLightSensor() {
+		return lightSensor.getVoltage();
 	}
 	
 	public double getEncoderPosition() {
@@ -31,7 +34,6 @@ public class Lift extends Manipulator {
 	}
 	
 	public void lift() {
-		lift.set(stick2.getRawAxis(1));
-		System.out.println("Lift Enc x: " + getEncoderPosition());
+		lift.set(-Math.abs(stick2.getRawAxis(1)));
 	}
 }
